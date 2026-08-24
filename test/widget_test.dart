daf680b388bc225ce8b3216f80a3cc3a553296ab
Sania Fida <saniafida@example.com>
2026-08-24 14:45:10 +0500

    updates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,14 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:color_match_trail/app/app.dart';
 import 'package:color_match_trail/core/services/service_locator.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
     TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
     await ServiceLocator.instance.initialize();
 
     await tester.pumpWidget(const ColorMatchTrailApp());
-    await tester.pump(const Duration(milliseconds: 300));
-
     expect(find.byType(ColorMatchTrailApp), findsOneWidget);
   });
 }
